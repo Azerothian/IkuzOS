db5213866857de84e506a7dd1ad57346da46714c
Brendon Allen <bdallen@Choji.local>
2010-05-26 16:56:41 +1000

    The start of the Kernel - Added GRUB Multiboot headers

---
+++ Kernel/main.c
@@ -0,0 +1,38 @@
+#define WHITE_SPACE 0x07
+#define VIDEO_MEMORY 0xb8000
+
+char *videoMemory = (char*) VIDEO_MEMORY;
+
+void clrscr()
+{
+    int i;
+    for(i=0;i < (80*25*2);i+=2)
+    {
+        videoMemory[i]=' ';
+        videoMemory[i+1]=WHITE_SPACE;
+    }
+}
+
+void printxy(char *message, unsigned int x, unsigned int y)
+{
+    unsigned int i=0;
+    i=(y*80*2)+x;
+    while(*message!=0)
+    {
+        if(*message=='\n')
+        {
+            y++;
+            i=(y*80*2);
+        } else {
+            videoMemory[i++]=*message;            
+            videoMemory[i++]=WHITE_SPACE;
+        }
+        *message++;
+    }
+}
+
+kernel_main()
+{
+    clrscr();
+    printxy("Hello World", 0, 0);
+}
