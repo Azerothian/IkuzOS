6a7a5a5afd09556dc869c55acf6e93eee6c81835
Brendon Allen <bdallen@Choji.local>
2010-05-27 10:06:19 +1000

    Added IO Functions inb and inw to k_io.c and k_io.h

--- Kernel/io/k_io.c
+++ Kernel/io/k_io.c
@@ -6,14 +6,42 @@
  */
 #include "k_io.h"
 
-
+/* Sends a byte (or word or dword) on a I/O location.
+ * Traditionnal names are outb, outw and outl respectively.
+ */
 void outb(unsigned short port, unsigned char data)
 {
     __asm__ ("outb %1, %0"::"d"(port),"a"(data));
 }
 
+/* Sends a byte (or word or dword) on a I/O location.
+ * Traditionnal names are outb, outw and outl respectively.
+ */
 void outw(unsigned short port, unsigned char data)
 {
     __asm__ ("outw %1, %0"::"d"(port),"a"(data));
 }
+
+/*
+ * Receives a byte (or word or dword) from an I/O location.
+ * Traditionnal names are inb, inw and inl respectively
+ */
+unsigned char inb(unsigned short port)
+{
+   unsigned char ret;
+   __asm__ volatile ("inb %1,%0":"=a"(ret):"Nd"(port));
+   return ret;
+}
+
+/*
+ * Receives a byte (or word or dword) from an I/O location.
+ * Traditionnal names are inb, inw and inl respectively
+ */
+unsigned char inw(unsigned short port)
+{
+   unsigned char ret;
+   __asm__ volatile ("inw %1,%0":"=a"(ret):"Nd"(port));
+   return ret;
+}
+
 
--- Kernel/io/k_io.h
+++ Kernel/io/k_io.h
@@ -7,4 +7,6 @@
 
 void outb(unsigned short port, unsigned char data);
 void outw(unsigned short port, unsigned char data);
+unsigned char inb(unsigned short port);
+unsigned char inw(unsigned short port);
 
