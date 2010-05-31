dabdcda6e9aea5d438e5b21add4564bf2a2c185f
Allen <ballen@itofficer.local>
2010-05-31 15:40:45 +1000

    Moved things around, made makefile more system independent.

---
+++ Kernel/arch/i386/boot.h
@@ -0,0 +1,10 @@
+/*
+ *  boot.h
+ *  Kernel
+ *
+ *  Created by Allen,Brendon on 31/05/10.
+ *  Copyright 2010 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+/* The Multiboot information.  */
--- Kernel/main.c
+++ Kernel/main.c
@@ -7,6 +7,7 @@
 #include <textmode.h>
 #include <fat.h>
 #include "drivers/ata/ata.h"
+#include "arch/i386/boot.h"
 
 /**
  * _kernel_main() - Main entry point for the IkuzOS Kernel
